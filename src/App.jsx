d87573d95286ef80b27260c4b46fae6465f18234
FelipeAndradeSZ <felipedosreis2002@gmail.com>
2025-11-09 20:44:20 -0300

    melhoria

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import {
   ChevronRight,
 } from "lucide-react";
 import { QRCodeSVG } from "qrcode.react";
+import estacioLogo from "./assets/logo.jpg";
 
 // ==== Serviços (Supabase) ====
 import {
@@ -429,8 +430,12 @@ const TelaLogin = ({ onLogin }) => {
     <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center p-4">
       <div className="bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md">
         <div className="text-center mb-8">
-          <div className="bg-blue-100 w-16 h-16 rounded-full flex items-center justify-center mx-auto mb-4">
-            <Users className="w-8 h-8 text-blue-600" />
+          <div className="mx-auto mb-4 flex justify-center">
+          <img
+          src={estacioLogo}
+          alt="Logo Estácio"
+          className="h-20 object-contain"
+          />
           </div>
           <h1 className="text-3xl font-bold text-gray-800">Sistema interno TI - Estacio RP</h1>
           <p className="text-gray-600 mt-2">Gestão de Infraestrutura</p>
